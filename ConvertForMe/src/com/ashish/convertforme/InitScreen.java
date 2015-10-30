4a4afc8c9b81a87a629f08f4b655644bb58630a0
Ashish Kalbhor <ashish.kalbhor@gmail.com>
2015-10-29 23:42:02 -0400

    Added InitScreen activity as splash screen
    - Need to add logic

---
+++ ConvertForMe/src/com/ashish/convertforme/InitScreen.java
@@ -0,0 +1,17 @@
+package com.ashish.convertforme;
+
+import android.app.Activity;
+
+/**
+ * 
+ * InitScreen is the Splash Screen that will be shown for this app.
+ * This screen should run only once, after installation.
+ * This screen contains information required to use the app.
+ * 
+ * @author Ashish Kalbhor
+ *
+ */
+public class InitScreen extends Activity
+{
+
+}
