a76534c9b5f1d55f37c04b8ea883d3cb9d404619
Ashish Kalbhor <ashish.kalbhor@gmail.com>
2015-10-31 01:48:26 -0400

    Working on InitScreen

--- ConvertForMe/src/com/ashish/convertforme/InitScreen.java
+++ ConvertForMe/src/com/ashish/convertforme/InitScreen.java
@@ -1,6 +1,9 @@
 package com.ashish.convertforme;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
 
 /**
  * 
@@ -13,5 +16,23 @@ import android.app.Activity;
  */
 public class InitScreen extends Activity
 {
-
+	private static int SPLASH_TIMEOUT = 3000;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_init);
+		
+		new Handler().postDelayed(new Runnable() 
+		{
+			@Override
+			public void run() 
+			{
+				Intent intent = new Intent(InitScreen.this, Convert.class);
+				startActivity(intent);
+				finish();
+			}
+		}, SPLASH_TIMEOUT);
+		
+	}
 }
